b4df00f8e9183dfc4c04832315b7eb51258fc6e3
Isak Mohammed <you@example.com>
2021-06-04 13:03:11 +0100

    update the visitcount function

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,17 +1,19 @@
-window.addEventListener('DOMContentLoaded',(event)=>{
+window.addEventListener('DOMContentLoaded',(event)=>{   /*when contenet gets loaded getvisitcount function will be called*/
     getVisitCount();
 })
 
+/*Defining the constant to store the APIGW url*/
 const functionApi = 'https://xodjnmk3i3.execute-api.eu-west-2.amazonaws.com/Prod/visitor-count/';
 
+/*JS function to grab the correct part of JSON provided by API and display in index.html */
 const getVisitCount = () => {
     fetch(functionApi)
-    .then(response =>response.text())
+    .then(response =>response.text()) /*grab the response of API*/
       .then((body) => {
-        var obj = JSON.parse(body);
-        document.getElementById("counter").innerHTML=obj.visitorCount
+        var obj = JSON.parse(body);   /*parse the json response in to variable obj*/
+        document.getElementById("counter").innerHTML=obj.visitorCount   /*grab the element 'counter' from index.html document and set it to value of visitorCount return by API(stored in variable obj) */
       })
-      .catch(function(error) {
+      .catch(function(error) {   /*Incase of error we will catch them to display in console*/
         console.log(error); 
       });  
     } 
